c9be8fa2b255f7abf1ce66558330109f68ca5a5c
M. Reza Ramadhan <rezaramadhan.m@gmail.com>
2016-09-25 10:18:44 +0700

    Add mutation function & part of selection function

--- Tubes1/src/tubes1/GeneticAlgorithm.java
+++ Tubes1/src/tubes1/GeneticAlgorithm.java
@@ -4,6 +4,7 @@
  * and open the template in the editor.
  */
 package tubes1;
+import java.util.Arrays;
 import java.util.Random;
 import java.util.List;
 /**
@@ -11,60 +12,202 @@ import java.util.List;
  * @author rezaramadhan
  */
 public class GeneticAlgorithm {
-    private List<Jadwal> matkul;
-    private List<Ruangan> ruang;
-    private String[] population;
+    private List<Jadwal> matkul; //list jadwal yg perlu diisi
+    private List<Ruangan> ruang; //list ruangan yang tersedia
+    private String[] population; //populasi tempat gen2 berkembang biak
     private int nPopulation;
 
-    public GeneticAlgorithm(List<Jadwal> k, List<Ruangan> r) {
+    public GeneticAlgorithm(List<Jadwal> k, List<Ruangan> r) { //konstruktor
             matkul = k;
             ruang = r;
+            population = new String[nPopulation];
     }
 
     //generate String random, representasi satu individu
     public String generateRandom() {
-            int totalGenerate = matkul.size();
-            String str = "";
+        int totalGenerate = matkul.size();
+        String str = "";
 
-            String tmp;
-            int max, min;
-            int idx, hari, mulai, selesai;
-            Random rand = new Random();
-            //format: <idxruang><Hari><mulai><selesai>
-            for (int i = 0; i < totalGenerate; i++) {
-                    tmp = "";
+         //format: <idxruang><Hari><mulai><selesai>
+        for (int i = 0; i < totalGenerate; i++) {
+            str = str + randomOneKelas(i);
+        }
+        return str;
+    }
+    
+    public String randomOneKelas(int idxMatkul) {
+        String tmp = "";
+        int max, min;
+        int idx, hari, mulai, selesai;
+        Random rand = new Random();
+        
+        
+        
+        //generate idxruang
+        idx = rand.nextInt(ruang.size());
+        String[] listruang = matkul.get(idx).Ruangan.split(",");
 
-                    //generate idxruang
-                    //TODO -> Cek barangkali kelasnya ga bisa di ruang yg diambil
-                    idx = rand.nextInt(ruang.size());
-                    tmp = tmp+idx;
+        while (!Arrays.asList(listruang).contains(ruang.get(idx).Nama)) {
+            idx = rand.nextInt(ruang.size());
+        }
 
-                    //generate hari
-                    hari = rand.nextInt(5) + 1;
-                    tmp = tmp + hari;
+        tmp = tmp+idx;
 
-                    min = Math.min(Integer.parseInt(matkul.get(i).JamMulai), Integer.parseInt(ruang.get(idx).JamMulai));
-                    max = Math.max(Integer.parseInt(matkul.get(i).JamSelesai), Integer.parseInt(ruang.get(idx).JamSelesai)) - Integer.parseInt(matkul.get(i).Durasi);
+        //generate hari
+        hari = rand.nextInt(5) + 1;
+        tmp = tmp + hari;
 
-                    //generate mulai
-                    mulai = rand.nextInt(max-min + 1) + min;
-                    tmp = tmp + mulai;
+        min = Math.min(Integer.parseInt(matkul.get(idxMatkul).JamMulai), Integer.parseInt(ruang.get(idx).JamMulai));
+        max = Math.max(Integer.parseInt(matkul.get(idxMatkul).JamSelesai), Integer.parseInt(ruang.get(idx).JamSelesai)) - Integer.parseInt(matkul.get(idxMatkul).Durasi);
 
-                    //generate selesai
-                    selesai = mulai + Integer.parseInt(matkul.get(i).Durasi);
-                    tmp = tmp + selesai;
+        //generate mulai
+        mulai = rand.nextInt(max-min + 1) + min;
+        tmp = tmp + mulai;
 
-                    str = str + tmp;
+        //generate selesai
+        selesai = mulai + Integer.parseInt(matkul.get(idxMatkul).Durasi);
+        tmp = tmp + selesai;
+        
+        return tmp;
+    } 
+
+    public String[] pisahstring(String str){
+        /* Memisahkan string panjang yang berisi daftar kelas beserta
+         * hari, jam mulai, selesai menjadi array yang berisi string
+         * dengan format
+         * iHms | iHms | iHms | ...
+         * dengan rincian:
+         * i = index matkul (a-z)
+         * H = hari dalam angka
+         * m = jam mulai (a-z) a:7, b:8, dst
+         * s = jam selesai (a-z) idem atas
+         */
+
+        //inisisalisasi tabJadwal 
+        String[] tabJadwal = new String [20];
+        for (int i=0; i<20; i++){
+                tabJadwal[i] = "";
+        }
+
+        //pisah string empat empat ke tabJadwal
+        int a = str.length();
+        int j=0;
+        int k=0;
+        String strtemp = "";
+        if ((a % 4)==0) {	//string kelipatan 4
+                for (int i=0; i<a; i++){
+                        if (j>3){
+                                j=0;
+                                tabJadwal[k]=strtemp;
+                                strtemp = "";
+                                k++;	
+                        }
+                        strtemp.concat(Character.toString(str.charAt(i)));
+                        j++;
+                }
+        } else {
+                //string salah, harus kelipatan 4
+                //tabJadwal kosong
+        }
+        return tabJadwal;
+    }
+
+    public float hitungFitnessFunction(String str){
+            /* Menghitung fitnessfunction dengan sebelumnya mengecek
+             * terjadinya bentrok matkul pada hari dan jam yang sama
+             * yaitu dengan mencatat matkul apa saja yang ada pada hari
+             * yang sama dengan menempatkannya pada matriks sbg berikut:
+             * baris menandakan hari
+             * kolom menandakan index matkul pada hari tsb
+             */
+
+            if ((str.length() % 4) == 0){
+                    String[] tabString = pisahstring(str);
+
+                    //inisialisasi tabHariSama
+                    String[][] tabHariSama = new String [5][5];
+                    for (int x=0; x<5; x++){
+                        for (int y=0; y<5; y++){
+                                tabHariSama[x][y] = "";
+                        }
+                    }
+
+                    //mengisi tabHariSama
+                    String strtemp ;
+                    int idxBrsHariSama = 0;
+                    int idxKolHariSama;
+                    for (char idxHari='a'; idxHari<='e'; idxHari++){
+                        idxKolHariSama = 0;
+                        for (int i=0; i< tabString.length; i++){
+                                strtemp = tabString[i];
+                                if (strtemp.charAt(1) == idxHari){	//mengecek kelas pada hari yg sama
+                                        tabHariSama[idxBrsHariSama][idxKolHariSama] = strtemp;
+                                        idxKolHariSama++;
+                                }
+                        }
+                        idxBrsHariSama++;
+                    }
+
+                    //Mengecek terjadinya bentrok berdasar tabHariSama
+                    String strtemp1 = "";
+                    int countBentrok = 0;
+                    int idxc;
+                    for (int idxa=0; idxa<5; idxa++){
+                        for(int idxb=0; idxb<5; idxb++){
+                            strtemp = tabHariSama[idxa][idxb];
+                            idxc=idxb+1;
+                            while (idxc<5){
+                                strtemp1 = tabHariSama[idxa][idxc];
+                                if ((	(strtemp.charAt(2)==strtemp1.charAt(2)) || 					//kedua kelas mulai pada jam sama
+                                            (strtemp.charAt(3)==strtemp1.charAt(3)) || 					//kedua kelas selesai pada jam sama
+                                            (strtemp.charAt(2)>strtemp1.charAt(2) && strtemp.charAt(2)<strtemp1.charAt(3)) || 	//kelas 1 mulai saat kelas 2 berlangsung
+                                            (strtemp.charAt(3)>strtemp1.charAt(2) && strtemp.charAt(3)<strtemp1.charAt(3))) 	//kelas 2 mulai saat kelas 1 berlangsung
+                                            && 
+                                            isSameRoom(strtemp,strtemp1)) {		//kedua kelas pada ruangan yang sama 
+                                    countBentrok++;
+                                }
+                                idxc++;
+                            }
+                        }
+                    }
+
+                    //mengembalikan nilai fitness function yaitu 1/countBentrok
+                    if (countBentrok == 0){	//tidak ada yg bentrok, menghindari infinite
+                        return 999;
+                    } else {
+                        return (1 / countBentrok);
+                    }
+            } else {
+                return -999; //error string tidak kelipatan 4
             }
-            return str;
+    }
+	
+    public boolean isSameRoom(String s1, String s2){
+        /* mengembalikan nilai kebenaran apakah kedua kelas berada
+         * pada ruangan yang sama
+         */
+        return (s1.charAt(0) == s2.charAt(0));
     }
 
-    public int fitnessFunction(String in) {
-
-    }
-
+    
     public String mutation (String in) {
-
+        /* 
+            mutation of one genome
+        */
+        char[] out = in.toCharArray();
+        int nKelas = matkul.size();
+        Random rand = new Random();
+        int mutatedPosition = rand.nextInt(nKelas + nKelas/2);
+        if (mutatedPosition < nKelas) {
+            int idxChange = (mutatedPosition - 1) * 4;
+            String tmp = randomOneKelas(mutatedPosition);
+            for (int i = 0; i<4; i++) {
+                out[idxChange] = tmp.charAt(i);
+                idxChange++;
+            }
+        }
+        String retVal = out.toString();
+        return retVal;
     }
 
     public String[] crossover(String str1, String str2) {
@@ -100,6 +243,23 @@ public class GeneticAlgorithm {
         return tabResult;
     }
 
+    public int selection() {
+        int selected;
+        
+        float[] fitnessFunction = new float[nPopulation];
+ 
+        //hitung fitness function buat setiap individu
+        for (int i= 0; i<nPopulation; i++) {
+            fitnessFunction[i] = hitungFitnessFunction(population[i]);
+        }
+        
+        //hitung persentasi setiap individu
+        
+        
+        
+        return selected;
+    }
+            
     public void fullProcess(){
 
     }
